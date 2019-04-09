7a40669d8b83700b7a177b05a3977341e24095b8
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-10 00:08:45 +0530

    Creaate basic directory structure

---
+++ src/main/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>hello world</header>
+      </div>
+    );
+  }
+}
+
+export default App;
