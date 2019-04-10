ef137b91594edc341aabc5d0c236ef4415ab704d
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-10 11:18:46 +0530

    Written basic tests and tests conventions

--- src/main/App.js
+++ src/main/App.js
@@ -4,8 +4,16 @@ import "./styles/App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>react version is {React.version}</header>
+      //the name of the attribut could be anything
+      //but the convention is to use data-test
+      <div className="App" data-test="component-app">
+        <header data-test="header">
+          react version is {React.version}
+        </header>
+        <h1 data-test="counter-display">The Count Is </h1>
+        <h1 data-test="counter-display">The Count Is </h1>
+        <h1 custom-test="counter-display">custom test</h1>
+        <button data-test="increment-button">Increment</button>
       </div>
     );
   }
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -7,4 +7,31 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders without crashing", () => {
   const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='header']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='counter-display']");
+  //here there is two componet-display that why the length should be 2
+  expect(appComponent.length).toBe(2);
+});
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='increment-button']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[custom-test='counter-display']");
+  expect(appComponent.length).toBe(1);
 });
