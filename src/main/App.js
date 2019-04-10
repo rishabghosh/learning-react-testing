1c1e0d93b45acd45bdfe1e364ea9e72984b15129
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-10 22:56:13 +0530

    simulated and tested a button click

--- src/main/App.js
+++ src/main/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./styles/App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  _handleIncrement() {
+    this.setState(prevState => {
+      return { count: prevState.count + 1 };
+    });
+  }
+
   render() {
     return (
       //the name of the attribut could be anything
       //but the convention is to use data-test
       <div className="App" data-test="component-app">
-        <header data-test="header">
-          react version is {React.version}
-        </header>
-        <h1 data-test="counter-display">The Count Is </h1>
-        <h1 data-test="counter-display">The Count Is </h1>
-        <h1 custom-test="counter-display">custom test</h1>
-        <button data-test="increment-button">Increment</button>
+        <header data-test="header">react version is {React.version}</header>
+        <h1 data-test="counter-display">The Count Is {this.state.count}</h1>
+        <h4 custom-test="counter-display">custom attribute works</h4>
+        <button
+          data-test="increment-button"
+          onClick={this._handleIncrement.bind(this)}
+        >
+          Increment
+        </button>
       </div>
     );
   }
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -5,33 +5,47 @@ import App from "../main/App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders without crashing", () => {
+test("renders app without crashing", () => {
   const wrapper = shallow(<App />);
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
 
-test("renders without crashing", () => {
+test("renders header without crashing", () => {
   const wrapper = shallow(<App />);
   const appComponent = wrapper.find("[data-test='header']");
   expect(appComponent.length).toBe(1);
 });
 
-test("renders without crashing", () => {
+test("renders display for counter without crashing", () => {
   const wrapper = shallow(<App />);
   const appComponent = wrapper.find("[data-test='counter-display']");
   //here there is two componet-display that why the length should be 2
-  expect(appComponent.length).toBe(2);
+  expect(appComponent.length).toBe(1);
 });
 
-test("renders without crashing", () => {
+test("renders button without crashing", () => {
   const wrapper = shallow(<App />);
   const appComponent = wrapper.find("[data-test='increment-button']");
   expect(appComponent.length).toBe(1);
 });
 
-test("renders without crashing", () => {
+test("renders custom test without crashing", () => {
   const wrapper = shallow(<App />);
   const appComponent = wrapper.find("[custom-test='counter-display']");
   expect(appComponent.length).toBe(1);
+});
+
+test("should increment on click", () => {
+  //gotcha set the same state that is used in the component
+  const count = 7;
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  //simulationg a click
+  wrapper.setState({ count });
+  button.simulate("click");
+  wrapper.update();
+  //checking if the display updates correctly or not
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.text()).toContain(count + 1);
 });
